a44b5bef249c951766771d1896f4e48950bacdb2
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-06-17 20:44:53 +0200

    sol

--- src/components/Input.js
+++ src/components/Input.js
@@ -34,41 +34,44 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React, {useState} from 'react'; /* STEP 0 */
+import React, { useState } from 'react' /* STEP 0 */
 
 export default function Input() {
   const [inputValue, setInputValue] = useState('')
   /* STEP 1 */
 
-  const changeInput = evt => {
+  const changeInput = (evt) => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
-    const { value } = evt.target;
-    setInputValue(inputValue(value))
+    const { value } = evt.target
+    setInputValue(value)
 
     /* STEP 4 */
-  };
+  }
   const reset = () => {
-    setInputValue('')
     /* STEP 5 */
-  };
+    setInputValue('')
+  }
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: inputValue.length > 10 ? 'crimson' : 'royalblue', /* STEP 2 */
-  };
+    color: inputValue.length > 10 ? 'crimson' : 'royalblue',
+  }
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
       <div id='output' style={style}>
-        {inputValue.toUpperCase}
-        </div> {/* STEP 3 */}
+        {inputValue.toUpperCase()}
+      </div>
       <div>
-        <input value={inputValue} id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
-        <button id='resetInput' onClick={reset}>Reset</button>
+        <input value={inputValue} id='input' type='text' onChange={changeInput} />{' '}
+        {/* STEP 6 */}
+        <button id='resetInput' onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
-  );
+  )
 }
