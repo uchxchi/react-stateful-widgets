156805a2aebb2b0726c8263d4e3e198527cdd61c
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-06-18 12:37:35 +0200

    sol

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -26,28 +26,28 @@ export const listOfAwesome = [
 ];
 
 export default function Programmers() {
-  const [ProgrammersList, setProgrammersList] = useState(listOfAwesome)
-  const [idOfFeatured, setIdOfFeatured] = useState(null)
+  const [programmers, setProgrammers] = useState(listOfAwesome)
+  const [programmerId, setProgrammerId] = useState(null)
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
 
   const getNameOfFeatured = () => {
+    const featured = programmers.filter(list => {
+      list.id == programmerId
+      return programmerId.name
+    })
+    console.log(featured)
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    const nameOfFeatured = ProgrammersList.filter(list => {
-    return  list.id === idOfFeatured
-    })
   };
-  // ProgrammersList[0].name
-  // console.log(nameOfFeatured)
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: idOfFeatured ? 'gold' : 'royalblue', // 🤔 color turns to gold, when celebrating
+    color: programmerId ? 'gold' : 'royalblue', // 🤔 color turns to gold, when celebrating
   };
 
   return (
@@ -58,9 +58,9 @@ export default function Programmers() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          ProgrammersList.map(dev =>
+          programmers.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { idOfFeatured(dev.id)/* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => { setProgrammerId(dev.id) /* in here set the featured id to be dev.id */ }}>Feature</button>
             </div>
           )
         }
@@ -70,7 +70,7 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          idOfFeatured 
+          programmerId
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
